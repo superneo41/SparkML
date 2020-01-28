5cbfea428137752a0555e6eba16c856021c76375
Neo <cyneo41@gmail.com>
2020-01-28 17:14:40 -0500

    Add files via upload

---
+++ sparkTuning.py
@@ -0,0 +1,19 @@
+from pyspark.ml.tuning import CrossValidator
+
+def tuning(model):
+  '''
+  a function to output the performance of different Hyper-parameter sets
+  
+  input: a CrossValidator fitted model
+  output: a dictionary with keys: hyper-para settings, values: metrics
+          sorted by value
+  '''
+  length = len(model.avgMetrics)
+  res = {}
+  for i in range(length):
+    s = ""
+    paraDict = model.extractParamMap()[model.estimatorParamMaps][i]
+    for j in paraDict.keys():
+      s += str(j).split("__")[1] + "  " + str(paraDict[j]) + "  "
+    res[s.strip()] = model.avgMetrics[i]
+  return {k: v for k, v in sorted(res.items(), key=lambda item: item[1])}
